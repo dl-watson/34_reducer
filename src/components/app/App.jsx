72ba6f74a6dfbb763eebb3b4a6a6945f1f77b0b8
dl-watson <dlarkinwatson@gmail.com>
2021-02-16 14:55:02 -0800

    caught one mistyped error in initialState.current, refactored onClick and onChange handlers to call dispatch() instead

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,80 +1,50 @@
 import React, { useReducer } from "react";
+import {
+  initialState,
+  ACTIONS,
+  colorReducer,
+} from "../../reducers/colorReducer";
 
-const ACTIONS = {
-  UNDO: "undo",
-  REDO: "redo",
-  RECORD: "record",
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case ACTIONS.UNDO:
-      return {
-        after: [state.current, ...state.after],
-        current: state.before[state.before.length - 1],
-        before: state.before.slice(0, -1),
-      };
-    case ACTIONS.REDO:
-      return {
-        before: [...state.before, state.current],
-        current: state.after[0],
-        after: state.after.slice(1),
-      };
-    case ACTIONS.RECORD:
-      return {
-        before: [...state.before, state.current],
-        current: action.payload,
-      };
-    default:
-      return state;
-  }
-};
-
-const useRecord = (init) => {
-  const [state, dispatch] = useReducer(reducer, {
-    before: [],
-    current: init,
-    after: [],
-  });
-
-  const undo = () => {
-    dispatch({ type: ACTIONS.UNDO });
-  };
-
-  const redo = () => {
-    dispatch({ type: ACTIONS.REDO });
-  };
-
-  const record = (val) => {
-    dispatch({ type: ACTIONS.CURRENT, payload: val });
-  };
+const App = () => {
+  const [state, dispatch] = useReducer(colorReducer, initialState);
 
   const { current } = state;
 
-  return {
-    undo,
-    record,
-    redo,
-    current,
-  };
-};
-
-const App = () => {
-  const { current, undo, redo, record } = useRecord("#FF0000");
-
   return (
     <>
-      <button onClick={undo}>undo</button>
-      <button onClick={redo}>redo</button>
+      <button
+        onClick={() =>
+          dispatch({
+            type: ACTIONS.UNDO,
+          })
+        }
+      >
+        undo
+      </button>
+      <button
+        onClick={() =>
+          dispatch({
+            type: ACTIONS.REDO,
+          })
+        }
+      >
+        redo
+      </button>
       <input
         type="color"
         data-testid="color-input"
         value={current}
-        onChange={({ target }) => record(target.value)}
+        onChange={({ target }) =>
+          dispatch({ type: ACTIONS.RECORD, payload: target.value })
+        }
       />
       <div
         data-testid="color-display"
-        style={{ backgroundColor: current, width: "10rem", height: "10rem" }}
+        style={{
+          backgroundColor: current,
+          width: "10rem",
+          height: "10rem",
+        }}
       ></div>
     </>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,31 @@
-const HtmlPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
-const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const CopyPlugin = require("copy-webpack-plugin");
 
 // eslint-disable-next-line
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.[hash].js',
-    publicPath: '/'
+    filename: "bundle.[hash].js",
+    publicPath: "/",
   },
   devServer: {
     port: 7891,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   plugins: [
-    new HtmlPlugin({ template: './src/index.html' }),
+    new HtmlPlugin({ template: "./src/index.html" }),
     new CleanWebpackPlugin(),
     new Dotenv({
-      systemvars: true
+      systemvars: true,
     }),
     new CopyPlugin({
-      patterns: [
-        { from: 'public' },
-      ]
-    })
+      patterns: [{ from: "public" }],
+    }),
   ],
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"],
   },
   module: {
     rules: [
@@ -35,49 +33,49 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            cacheDirectory: true
-          }
-        }
+            cacheDirectory: true,
+          },
+        },
       },
       {
         test: /\.css$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: "style-loader",
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               sourceMap: true,
               modules: true,
-              importLoaders: 1
-            }
+              importLoaders: 1,
+            },
           },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               sourceMap: true,
               postcssOptions: {
                 plugins: [
-                  require('postcss-import')(),
-                  require('autoprefixer')(),
-                  require('postcss-nested')(),
-                  require('postcss-simple-vars')()
-                ]
-              }
-            }
-          }
-        ]
+                  require("postcss-import")(),
+                  require("autoprefixer")(),
+                  require("postcss-nested")(),
+                  require("postcss-simple-vars")(),
+                ],
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.(jpeg|jpg|png|svg)$/,
         use: {
-          loader: 'url-loader',
+          loader: "url-loader",
           options: { limit: 1000 },
         },
-      }
-    ]
-  }
+      },
+    ],
+  },
 };
